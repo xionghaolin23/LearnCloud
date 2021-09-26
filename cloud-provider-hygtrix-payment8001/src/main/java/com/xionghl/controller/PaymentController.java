3830591f12c47ef0816e203dab0e20beda62a68f
xionghaolin <1445695649@qq.com>
2021-09-26 21:54:37 +0800

    feat: Hystrix服务熔断  by 熊浩淋

--- cloud-provider-hygtrix-payment8001/src/main/java/com/xionghl/controller/PaymentController.java
+++ cloud-provider-hygtrix-payment8001/src/main/java/com/xionghl/controller/PaymentController.java
@@ -35,4 +35,17 @@ public class PaymentController {
         log.info("*****result: " + result);
         return result;
     }
+
+    /**
+     * 服务熔断
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("****result: " + result);
+        return result;
+    }
 }
